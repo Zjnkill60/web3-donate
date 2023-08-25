9aaf085537c0aa605621f19d705edbe3f5cdc501
DUCANH <zjnkill18@gmail.com>
2023-08-25 11:08:47 +0700

    3

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     const abi = await fetch('/contracts/Faucet.json')
     const abiJSON = await abi.json()
 
-    const contract = new web3.eth.Contract([abiJSON], "0x7af2b76997D1Fff2A52B96487B3d7E9EF05AB42d")
+    const contract = new web3.eth.Contract(abiJSON.abi, "0x7af2b76997D1Fff2A52B96487B3d7E9EF05AB42d")
     contract.setProvider(provider)
     console.log(contract)
     if (provider) {
@@ -33,7 +33,7 @@ function App() {
 
   const getAccount = async () => {
     let myAddress = await web3Api.web3?.eth.getAccounts()
-    let myBalance = await web3Api.web3?.eth.getBalance(web3Api?.contract?._address)
+    let myBalance = await web3Api.web3?.eth.getBalance(web3Api?.contract?._address) / 1e18
     setAddress(myAddress ? myAddress : null)
     setBalance(myBalance ? myBalance : null)
 
@@ -48,10 +48,19 @@ function App() {
 
   const addFunds = async () => {
     const { contract, web3 } = web3Api
-    await contract.addFunds({
-      from: address,
-      value: web3.utils.toWei('1', 'ether')
+    await contract.methods.addFunds().send({
+      from: address[0],
+      value: web3.utils.toWei('0.01', 'ether')
     })
+    getAccount()
+  }
+
+  const widthraw = async () => {
+    const { contract, web3 } = web3Api
+    await contract.methods.withdraw(web3.utils.toWei('0.01', 'ether')).send({
+      from: address[0],
+    })
+    getAccount()
   }
 
 
@@ -70,10 +79,10 @@ function App() {
   return (
     <div style={{ height: '100vh', display: 'grid', placeItems: 'center' }}>
       <div>
-        <div style={{ fontSize: 30 }}>Current Banlance Donate : <span style={{ fontWeight: 600 }}>{balance} ONUS</span></div>
+        <div style={{ fontSize: 30 }}>Current Banlance Donate : <span style={{ fontWeight: 600 }}>{balance ? balance : 0} ONUS</span></div>
         <div style={{ margin: '20px 0' }}>
           <button onClick={addFunds} type="button" class="btn btn-success">Donate</button>
-          <button style={{ margin: '0 10px' }} type="button" class="btn btn-danger">Withdraw</button>
+          <button onClick={widthraw} style={{ margin: '0 10px' }} type="button" class="btn btn-danger">Withdraw</button>
           {address?.length > 0 ? <button type="button" class="btn btn-primary">Login Success !</button> :
             <button onClick={() => loginMetaMask()} type="button" class="btn btn-primary">Connect Wallet</button>}
         </div>
